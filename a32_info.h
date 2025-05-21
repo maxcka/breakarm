203bcf22d61139bde38cdc36d020768b23d96d13
max <maxckallas@gmail.com>
2025-05-20 21:59:42 -0700

    - using bitmasks to check instruction fields

--- a32_info.h
+++ a32_info.h
@@ -5,26 +5,38 @@
 // A32 instructions are 4 bytes wide
 #define A32_INSTR_SIZE 4
 
-// encoding
+// decoding
+// 1. check cond
+// 2. check op1
+// 3. check op
 
-//=== cond field (bits 28 - 31)
+//=== cond field (bits 31 - 28)
 
 // |31 30 29 28|27 26 25|24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5| 4 |3 2 1 0|
 // |  cond     |  op1   |                                                      |op |       |
 
 #define UNCOND  // 0b111
 
-//=== op1 field (bits 25 - 27)
+//===
 
+//=== op1 field (bits 27 - 25)
+// Note: because Branch and Coprocessor / Supervisor instructions only use bits 27 and 26,
+// only check those first
+
+//--> mask bits 27-25
 // Data-processing (register)
-#define DATA_PROC_REG // 0b000
+#define IS_DATA_PROC_REG(instr)     ( ( ((instr) >> 25) & 0x7) == 0x0 ) // 0b000
 // Data-processing (immediate)
-#define DATA_PROC_IMM // 0b001
-
-//#define // 0b010
-//#define // 0b011
-//#define // 0b10
-//#define // 0b11
+#define IS_DATA_PROC_IMM(instr)     ( ( ((instr) >> 25) & 0x7) == 0x1 ) // 0b001
+// load/store word and unsigned byte (usually immediate)
+#define IS_LD_STR_WRD(instr)        ( ( ((instr) >> 25) & 0x7) == 0x2 ) // 0b010
+// load/store word and unsigned byte (usually register) or media instr
+#define IS_LD_STR_WRD_MEDIA(instr)  ( ( ((instr) >> 25) & 0x7) == 0x3 ) // 0b011
+//--> mask bits 27-26
+// branch, branch with link, and block data transfer
+#define IS_BR_BLK(instr)            ( ( ((instr) >> 26) & 0x7) == 0x2 ) // 0b10
+// Coprocessor instr, and supervisor call
+#define IS_CPROC_SPR(instr)         ( ( ((instr) >> 26) & 0x7) == 0x3 ) // 0b11
 
 //=== 
 
