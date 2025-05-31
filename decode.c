e16bd0654145867504453d933dc6d7f35504a2c3
max <maxckallas@gmail.com>
2025-05-30 21:09:27 -0700

    - cleaned up README - added instruction types and a print function to handle instructions with different syntax

--- decode.c
+++ decode.c
@@ -52,6 +52,34 @@ void get_shift_str(Shift shift, char *shift_str, int buf_sz) {
 }
 
 
+void print_asm_instr(Instr *instr_s) {
+    switch (instr_s->i_type) {
+        case TYPE_0: // syntax: MNEMONIC{S}<c> <Rd>, <Rn>, <Rm>{, <shift>}
+            printf("%s%s%s %s, %s%s %s%s\n", 
+            instr_s->mnemonic, 
+            (instr_s->S) ? "S" : "", 
+            cond_codes[instr_s->c], 
+            core_reg[instr_s->Rd], 
+            (instr_s->special) ? "" : core_reg[instr_s->Rn], 
+            (instr_s->special) ? "" : ",", 
+            core_reg[instr_s->Rm], 
+            instr_s->shift_str);
+
+            break;
+        
+        case TYPE_1: // syntax: MNEMONIC<c> <Rn>, <Rm>{, <shift>}
+            printf("%s%s %s, %s%s\n", 
+            instr_s->mnemonic, 
+            cond_codes[instr_s->c], 
+            core_reg[instr_s->Rn], 
+            core_reg[instr_s->Rm], 
+            instr_s->shift_str);
+
+            break;
+    }
+}
+
+
 // ====================
 // === INSTRUCTIONS ===
 // ====================
@@ -74,16 +102,14 @@ void process_data_proc_instr(uint32_t instr, Instr *instr_s) {
 
 
     // special case Encoding A2: <opc2>S{<c>}{<q>} <Rd>, <Rm> {, <shift>}
-    if (instr_s->S == 0x1 && instr_s->Rd == PC) {
+    if (instr_s->i_type == TYPE_0 && instr_s->S == 0x1 && instr_s->Rd == PC) {
         instr_s->special = 1;
         instr_s->mnemonic = "MVN";
     }
 
     get_shift_str(instr_s->shift, instr_s->shift_str, BUF_20);
 
-    printf("%s%s%s %s, %s%s %s%s\n", instr_s->mnemonic, (instr_s->S) ? "S" : "", 
-        cond_codes[instr_s->c], core_reg[instr_s->Rd], (instr_s->special) ? "" : core_reg[instr_s->Rn], 
-        (instr_s->special) ? "" : ",", core_reg[instr_s->Rm], instr_s->shift_str);
+    print_asm_instr(instr_s);
 
 }
 
@@ -126,6 +152,12 @@ void AND_reg_instr(uint32_t instr) {
 }
 */
 // process EOR (register) instruction
+// syntax: EOR{S}{<c>}{<q>} {<Rd>,} <Rn>, <Rm> {, <shift>}
+void EOR_reg_instr(uint32_t instr) {
+    Instr instr_s = {0};
+    instr_s.mnemonic = "EOR";
+    process_data_proc_instr(instr, &instr_s);
+}
 
 // process SUB (register) instruction
 // syntax: SUB{S}{<c>}{<q>} {<Rd>,} <Rn>, <Rm> {, <shift>}
@@ -136,6 +168,12 @@ void SUB_reg_instr(uint32_t instr) {
 }
 
 // process RSB (register) instruction
+// syntax: RSB{S}{<c>}{<q>} {<Rd>,} <Rn>, <Rm> {, <shift>}
+void RSB_reg_instr(uint32_t instr) {
+    Instr instr_s = {0};
+    instr_s.mnemonic = "RSB";
+    process_data_proc_instr(instr, &instr_s);
+}
 
 // process ADD (register, ARM) instruction
 // syntax: ADD{S}{<c>}{<q>} {<Rd,} <Rn>, <Rm> {, <shift>}
@@ -174,10 +212,48 @@ void ADD_reg_instr(uint32_t instr) {
 }
 */
 // process ADC (register) instruction
+// syntax: ADC{S}{<c>}{<q>} {<Rd>,} <Rn>, <Rm> {, <shift>}
+void ADC_reg_instr(uint32_t instr) {
+    Instr instr_s = {0};
+    instr_s.mnemonic = "ADC";
+    process_data_proc_instr(instr, &instr_s);
+}
 
 // process SBC (register) instruction
+// syntax: SBC{S}{<c>}{<q>} {<Rd>,} <Rn>, <Rm> {, <shift>}
+void SBC_reg_instr(uint32_t instr) {
+    Instr instr_s = {0};
+    instr_s.mnemonic = "SBC";
+    process_data_proc_instr(instr, &instr_s);
+}
 
 // process RSC (register) instruction
+// syntax: RSC{S}{<c>}{<q>} {<Rd>,} <Rn>, <Rm> {, <shift>}
+void RSC_reg_instr(uint32_t instr) {
+    Instr instr_s = {0};
+    instr_s.mnemonic = "RSC";
+    process_data_proc_instr(instr, &instr_s);
+}
+
+// process TST (register) instruction
+void TST_reg_instr(uint32_t instr) {
+    Instr instr_s = {0};
+    instr_s.mnemonic = "TST";
+    instr_s.i_type = TYPE_1;
+    process_data_proc_instr(instr, &instr_s);
+}
+// process TEQ (register) instruction
+// process CMP (register) instruction
+// process CMN (register) instruction
+// process ORR (register) instruction
+// process MOV (register) instruction
+// process LSL (immediate) instruction
+// process LSR (immediate) instruction
+// process ASR (immediate) instruction
+// process RRX instruction
+// process ROR (immediate) instruction
+// process BIC (register) instruction
+// process MVN (register) instruction
 
 
 
@@ -190,12 +266,36 @@ void decode_dp_reg(uint32_t instr) {
     if (IS_AND_REG(instr)) {
         AND_reg_instr(instr); // process AND (register) instr
     }
+    else if (IS_EOR_REG(instr)) {
+        EOR_reg_instr(instr);
+    }
     else if (IS_SUB_REG(instr)) {
         SUB_reg_instr(instr);
+    }
+    else if (IS_RSB_REG(instr)) {
+        RSB_reg_instr(instr);
     }
     else if (IS_ADD_REG(instr)) {
         ADD_reg_instr(instr);
     }
+    else if (IS_ADC_REG(instr)) {
+        ADC_reg_instr(instr);
+    }
+    else if (IS_SBC_REG(instr)) {
+        SBC_reg_instr(instr);
+    }
+    else if (IS_RSC_REG(instr)) {
+        RSC_reg_instr(instr);
+    }
+    else if (IS_TST_REG(instr)) {
+        TST_reg_instr(instr);
+    }
+    //else if (IS_TEQ_REG(instr)) {
+    //    TEQ_reg_instr(instr);
+    //}
+    //else if (IS_CMP_REG(instr)) {
+    //    CMN_reg_instr(instr);
+    //}
     else {
         printf("%s\n", default_str);
     }
--- decode.h
+++ decode.h
@@ -63,7 +63,13 @@ typedef enum {
     AL
 } Cond;
 
+typedef enum {
+    TYPE_0, // syntax: MNEMONIC{S}<c> <Rd>, <Rn>, <Rm>{, <shift>}
+    TYPE_1, // syntax: MNEMONIC<c> <Rn>, <Rm>{, <shift>}
+} Instr_type;
+
 typedef struct {
+    Instr_type i_type; // instruction type
     uint8_t special;
     const char *mnemonic;
     Shift shift;
@@ -74,6 +80,8 @@ typedef struct {
     Register Rm;
     uint8_t S;
 } Instr;
+
+
 
 //==========================================
 //==========================================
@@ -103,22 +111,45 @@ typedef struct {
 // only check those first
 
 //--> mask bits 27-25
-// Data-processing (register)
+
 //>> layer 1
+// Data-processing (register) & miscellaneous instructions
 #define IS_DP_OP_0(instr)       ( ( ((instr) >> 25) & 0x7) == 0x0 ) // 0b000
 //>> layer 2
 // instruction is data-processing (register) or data-processing(register-shifted register)
 #define IS_DP_REG_OR_RSR(instr) ( ( ((instr) >> 20) & 0x19) != 0xF ) // not 0b10xx0
 //>> layer 3
-// instr is data-processing (register)
+//=== instr is data-processing (register) ===
 #define IS_DP_REG(instr)        ( ( ((instr) >> 4) & 0x1) == 0x0) // 0bxxx0
 //>> layer 4
 // instr is AND (register) instruction
 #define IS_AND_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0x0) // 0b0000x
-//#define IS_EOR_REG(instr)
+#define IS_EOR_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0x2) // 0b0001x
 #define IS_SUB_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0x4) // 0b0010x
-//#define IS_RSB_REG(instr)
+#define IS_RSB_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0x6) // 0b0011x
 #define IS_ADD_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0x8) // 0b0100x
+#define IS_ADC_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0xA) // 0b0101x
+#define IS_SBC_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0xC) // 0b0110x
+#define IS_RSC_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0xE) // 0b0111x
+// 0b10xx0
+#define IS_TST_REG(instr)       ( ( ((instr) >> 20) & 0x1F) == 0x11) // 0b10001
+#define IS_TEQ_REG(instr)       ( ( ((instr) >> 20) & 0x1F) == 0x13) // 0b10011
+#define IS_CMP_REG(instr)       ( ( ((instr) >> 20) & 0x1F) == 0x15) // 0b10101
+#define IS_CMN_REG(instr)       ( ( ((instr) >> 20) & 0x1F) == 0x17) // 0b10111
+#define IS_ORR_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0x18) // 0b1100x
+//--- move (reg), shift (imm), and rotate (imm) instructions ---
+#define IS_MV_OR_SHFT(instr)    ( ( ((instr) >> 20) & 0x1E) == 0x1A) // 0b1101x
+#define IS_MOV_REG(instr)       ( ( ( ((instr) >> 5) & 0x3) == 0x0) && ( ( ((instr) >> 7) & 0x1F) == 0x0) ) // 0b00 & 0b00000
+#define IS_LSL_IMM(instr)       ( ( ( ((instr) >> 5) & 0x3) == 0x0) && ( ( ((instr) >> 7) & 0x1F) != 0x0) ) // 0b00 & not 0b00000
+#define IS_LSR_IMM(instr)       ( ( ((instr) >> 5) & 0x3) == 0x1) // 0b01
+#define IS_ASR_IMM(instr)       ( ( ((instr) >> 5) & 0x3) == 0x2) // 0b10
+#define IS_RRX(instr)           ( ( ( ((instr) >> 5) & 0x3) == 0x3) && ( ( ((instr) >> 7) & 0x1F) == 0x0) ) // 0b11 & 0b00000
+#define IS_ROR_IMM(instr)       ( ( ( ((instr) >> 5) & 0x3) == 0x3) && ( ( ((instr) >> 7) & 0x1F) != 0x0) ) // 0b11 & not 0b00000
+//---------------------------------------------------------------
+#define IS_BIC_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0x1C) // 0b1110x
+#define IS_MVN_REG(instr)       ( ( ((instr) >> 20) & 0x1E) == 0x1E) // 0b1111x
+//============================================
+
 //=======================
 
 
